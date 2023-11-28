a66812d297c81f3e088fd6acd0faa6082dd5215d
Brad Caron <bacaron@utexas.edu>
2023-11-28 16:46:51 -0600

    Reason: update to be more generic with tract names as to allow for comparison between different segmentations

--- wmc2trk.py
+++ wmc2trk.py
@@ -28,8 +28,9 @@ def wmc2trk(trk_file, classification, tractID_list, out_dir):
     hdr['voxel_to_rasmm'] = aff_vox_to_ras
 
     for tractID in tractID_list:
-    	t_name = data['names'][0][tractID-1][0]
-    	tract_name = t_name.replace(' ', '_')
+    	# t_name = data['names'][0][tractID-1][0]
+    	# tract_name = t_name.replace(' ', '_')
+        tract_name = str(tractID)
     	idx_tract = np.array(np.where(indeces==tractID))[0]
     	tract = tractogram[idx_tract]
 
